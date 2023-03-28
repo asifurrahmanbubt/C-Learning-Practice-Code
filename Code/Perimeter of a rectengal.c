901c4e7276da4b702215c2bae8f2288fca511fdb
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-28 23:46:58 +0600

    Add files via upload

---
+++ Code/Perimeter of a rectengal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//A program to calculate perimeter of a rectangle
+int main() {
+
+        float length, width, perimeter;
+
+        printf("Enter the value of Length : ");
+        scanf("%f", &length);
+        printf("Enter the value of Width : ");
+        scanf("%f", &width);
+
+        perimeter = 2 * ( legnth + width );
+        printf("The Perimeter of the rectangle is : %f units", perimeter);
+
+        return 0;
+
+}
