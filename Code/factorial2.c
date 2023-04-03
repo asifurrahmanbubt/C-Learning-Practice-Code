d0668faf0cb2fdecf6031465198462aee68e3dbc
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-04-03 23:54:54 +0600

    Add files via upload

---
+++ Code/factorial2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,fact=1,i;
+
+    printf("Enter a number : ");
+    scanf("%d", &a);
+
+ do{ fact*=i;
+ a--;
+ }
+ while (a > 0);
+    printf("The Factorial of the number is : %d", fact);
+
+    return 0;
+
+}
